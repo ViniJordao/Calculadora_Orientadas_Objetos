37de46902d239dbef65d6d0810d2867fa83506ef
ViniJordao <viniciusjordao02@gmail.com>
2021-04-21 23:26:42 -0300

    Add project files.

---
+++ Calculadora.OO/Calculos.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora.OO
+{
+    class Calculos
+    {
+        public string[] operacoesRealizadas = new string[50];
+        public int operacoesFeitas = 0;
+        public string simboloOperacao = "";
+        public string opcao = "";
+        public double primeiroNumero, segundoNumero;
+        public double resultado = 0;
+
+        public void Operacoes(string opcao, double resultado, double primeiroNumero, double segundoNumero, string simboloOperacao)
+        {
+
+            switch (opcao)
+            {
+                case "1":
+
+                    resultado = primeiroNumero + segundoNumero;
+                    simboloOperacao = "+";
+                    Console.WriteLine("A soma dos numeros Totalizaram: " + resultado); break;
+                case "2":
+
+                    resultado = primeiroNumero - segundoNumero;
+                    simboloOperacao = "-";
+                    Console.WriteLine("a subtração totalizou: " + resultado); break;
+                case "3":
+
+                    resultado = primeiroNumero * segundoNumero;
+                    simboloOperacao = "*";
+                    Console.WriteLine("o Resultado da sua Multiplicação foi: " + resultado); break;
+                case "4":
+
+                    if (segundoNumero == 0)
+                    {
+                        Console.WriteLine("Atenção na Operação de Divisão do segundo numero não pode ser igual ha 0");
+                        break;
+                    }
+                    resultado = primeiroNumero / segundoNumero;
+                    simboloOperacao = "/";
+                    Console.WriteLine("O resultado da sua divisão foi : " + resultado); break;
+
+            }
+        }
+        public bool OpcaoSair(string opcao)
+        {
+            return opcao.Equals("s", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public bool OpcaodeVisualizar(string opcao)
+        {
+            return opcao == "5";
+        }
+
+        public bool OpcaoEscolha(string opcao)
+        {
+            return opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "5" && opcao != "S" && opcao != "s";
+        }
+
+        public void Menu()
+        {
+            Console.WriteLine("Calculadora Tabajara");
+
+            Console.WriteLine();          
+
+            Console.WriteLine("Digite 1 para somar");
+
+            Console.WriteLine("Digite 2 para subtrair");
+
+            Console.WriteLine("Digite 3 para multiplicação");
+
+            Console.WriteLine("Digite 4 para divisão");
+
+            Console.WriteLine("Digite 5 para visualizar as operações realizadas");
+
+            Console.WriteLine("Digite S para sair");
+
+        }
+    }
+}
---
+++ Calculadora.OO/Program.cs
@@ -0,0 +1,76 @@
+﻿using Calculadora.OO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraPOO.consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculos calc = new Calculos();
+            while (true)
+            {
+                calc.Menu();
+
+                calc.opcao = Console.ReadLine();
+
+                if (calc.OpcaoEscolha(calc.opcao))
+                {
+                    Console.WriteLine("Opcao Inválida");
+                    Console.ReadLine();
+                    Console.Clear();
+                    continue;
+                }
+                if (calc.OpcaodeVisualizar(calc.opcao))
+                {
+                    if (calc.operacoesFeitas == 0)
+                    {
+                        Console.WriteLine("Nenhuma operação foi feita até o momento.");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < calc.operacoesRealizadas.Length; i++)
+                        {
+                            if (calc.operacoesRealizadas[i] != null)
+                                Console.WriteLine(calc.operacoesRealizadas[i]);
+                        }
+                    }
+                    Console.ReadLine();
+                    Console.Clear();
+
+                    continue;
+                }
+
+                if (calc.OpcaoSair(calc.opcao))
+                {
+                    break;
+                }
+
+                Console.Write("Digite o primeiro número: ");
+                calc.primeiroNumero = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o segundo número: ");
+                calc.segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+                calc.Operacoes(calc.opcao,  calc.resultado, calc.primeiroNumero, calc.segundoNumero, calc.simboloOperacao);
+
+                string operacaoRealizada =
+                    calc.primeiroNumero.ToString() + " " + calc.simboloOperacao + " " +
+                    calc.segundoNumero.ToString() + " = " + calc.resultado.ToString();
+
+                calc.operacoesRealizadas[calc.operacoesFeitas] = operacaoRealizada;
+
+                calc.operacoesFeitas++;
+
+                Console.WriteLine();
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+        }
+
+    }
+}
